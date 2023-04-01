88c3d58e04aeba468532d02877daf4e0fbebda20
shendel <im_shendel@inbox.ru>
2023-04-01 12:57:09 +0300

    init

---
+++ _next/static/Gkt9Su-ocj1SrPKwUChvp/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/542b50fd-07ebedc579cef971.js","static/chunks/769-e925041f0f05658b.js","static/chunks/pages/index-de701a2a00bb91eb.js"],"/_error":["static/chunks/pages/_error-3f6d1c55bb8051ab.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
